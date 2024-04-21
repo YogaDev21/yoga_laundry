d9f42459e1da686cb77375380016b06b3f1d6f38
yogadev21 <yogadev2006@gmail.com>
2024-04-21 12:50:53 +0800

    feat: flash message after delete

--- php/delete/delete_transaksi.php
+++ php/delete/delete_transaksi.php
@@ -1,10 +1,18 @@
 <?php
+session_start();
 include "../../koneksi.php";
-
 $id = $_GET["id"];
-$query = "DELETE FROM tb_transaksi WHERE id = $id";
-$push = mysqli_query($koneksi, $query);
-
-if ($push) {
-  header('location: ../view/control.php?page=view_transaksi');
+try {
+  $query = "DELETE FROM tb_transaksi WHERE id = $id";
+  $push = mysqli_query($koneksi, $query);
+  if ($push) {
+    $_SESSION['status'] = "Transaksi Berhasil Dihapus";
+    header('location: ../view/control.php?page=view_transaksi');
+  }
+} catch (mysqli_sql_exception $e) {
+  $_SESSION['status'] = "Transaksi Gagal Dihapus";
+  echo "<script>
+    alert('Terjadi kesalahan, coba lagi nanti.');
+    window.history.back();
+  </script>";
 }
